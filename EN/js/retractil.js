921711c8568937291c3b2ee6a02aa588e4fb3f2d
Santiago <sjmunoz1@uc.cl>
2019-05-17 11:48:04 -0400

    entregaCata

---
+++ EN/js/retractil.js
@@ -0,0 +1,119 @@
+jQuery(document).ready(function ($) {
+
+  $("#toggle").click(function () {
+    $("#wrap").slideToggle("normal");
+
+    if ($("#toggle").text() == "Transporte marítimo ") {
+      $("#toggle").html("Transporte marítimo  <span class='fa fa-angle-double-down'></span>")
+    }
+    else {
+      $("#toggle").html("Transporte marítimo <span class='fa fa-angle-double-right'></span>")
+
+    }
+
+  });
+
+});
+
+
+jQuery(document).ready(function ($) {
+
+  $("#toggle2").click(function () {
+    $("#wrap2").slideToggle("normal");
+
+    if ($("#toggle2").text() == "Transporte aéreo ") {
+      $("#toggle2").html("Transporte aéreo  <span class='fa fa-angle-double-down'></span>")
+    }
+    else {
+      $("#toggle2").html("Transporte aéreo <span class='fa fa-angle-double-right'></span>")
+
+    }
+
+  });
+
+});
+
+
+jQuery(document).ready(function ($) {
+
+  $("#toggle3").click(function () {
+    $("#wrap3").slideToggle("normal");
+
+    if ($("#toggle3").text() == "Transporte terrestre ") {
+      $("#toggle3").html("Transporte terrestre  <span class='fa fa-angle-double-down'></span>")
+    }
+    else {
+      $("#toggle3").html("Transporte terrestre <span class='fa fa-angle-double-right'></span>")
+
+    }
+
+  });
+
+});
+
+jQuery(document).ready(function ($) {
+  
+  $("#toggle4").click(function () {
+    $("#wrap4").slideToggle("normal");
+
+    if ($("#toggle4").text() == " Other background: ") {
+      $("#toggle4").html("<span class='fa fa-angle-right inamovible'></span> Other background:  <span class='fa fa-angle-double-down'></span>")
+    }
+    else {
+      $("#toggle4").html("<span class='fa fa-angle-right inamovible'></span> Other background: <span class='fa fa-angle-double-right'></span>")
+
+    }
+
+  });
+
+});
+
+jQuery(document).ready(function ($) {
+
+  $("#toggle5").click(function () {
+    $("#wrap5").slideToggle("normal");
+
+    if ($("#toggle5").text() == " Other background: ") {
+      $("#toggle5").html("<span class='fa fa-angle-right inamovible'></span> Other background:  <span class='fa fa-angle-double-down'></span>")
+    }
+    else {
+      $("#toggle5").html("<span class='fa fa-angle-right inamovible'></span> Other background: <span class='fa fa-angle-double-right'></span>")
+
+    }
+
+  });
+
+});
+
+jQuery(document).ready(function ($) {
+
+  $("#toggle6").click(function () {
+    $("#wrap6").slideToggle("normal");
+
+    if ($("#toggle6").text() == " Other background: ") {
+      $("#toggle6").html("<span class='fa fa-angle-right inamovible'></span> Other background:  <span class='fa fa-angle-double-down'></span>")
+    }
+    else {
+      $("#toggle6").html("<span class='fa fa-angle-right inamovible'></span> Other background: <span class='fa fa-angle-double-right'></span>")
+
+    }
+
+  });
+
+});
+jQuery(document).ready(function ($) {
+
+  $("#toggle7").click(function () {
+    $("#wrap7").slideToggle("normal");
+
+    if ($("#toggle7").text() == " Other background: ") {
+      $("#toggle7").html("<span class='fa fa-angle-right inamovible'></span> Other background:  <span class='fa fa-angle-double-down'></span>")
+    }
+    else {
+      $("#toggle7").html("<span class='fa fa-angle-right inamovible'></span> Other background: <span class='fa fa-angle-double-right'></span>")
+
+    }
+
+  });
+
+});
